286b8e4b26bafa6d623375d632e14f4168306080
PokMan Chung <2512232c@student.gla.ac.uk>
2021-02-05 12:46:35 +0000

    first commit

--- populate_rango.py
+++ populate_rango.py
@@ -30,17 +30,15 @@ def populate():
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages, 'views': views, 'likes': likes},
-        'Django': {'pages': django_pages, 'views': views, 'likes': likes},
-        'Other Frameworks': {'pages': other_pages, 'views': views, 'likes': likes}}
-
-    views = {'Python': 'views',
-        'Django' : 'views',
-        'Other Frameworks': 'views'}
-
-    likes = {'Python': 'likes',
-        'Django' : 'like',
-        'Other Frameworks': 'like'}
+    cats = {'Python': {'pages': python_pages, 
+                        'views': 128, 
+                        'likes': 64},
+        'Django': {'pages': django_pages, 
+                    'views': 64, 
+                    'likes': 32},
+        'Other Frameworks': {'pages': other_pages, 
+                            'views': 32, 
+                            'likes': 16}}
 
     for cat, cat_data in cats.items():
         c = add_cat(cat, cat_data['views'], cat_data['likes'])
